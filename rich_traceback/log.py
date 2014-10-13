aea5dc842e088cda395ceb968d35cbb23614d827
Laurentiu Badea <laurb9@gmail.com>
2014-10-12 22:39:25 -0700

    Move license to LICENSE

--- rich_traceback/log.py
+++ rich_traceback/log.py
@@ -3,22 +3,7 @@
 # Copyright (C)2001-2002 InfoStreet, Inc.
 #                        L.C. (Laurentiu C. Badea) lc@infostreet.com
 # GPL-ed on Mar 19, 2003
-#
-## This program is free software; you can redistribute it and/or modify
-## it under the terms of the GNU General Public License as published by
-## the Free Software Foundation; either version 2 of the License, or
-## (at your option) any later version.
- 
-## This program is distributed in the hope that it will be useful,
-## but WITHOUT ANY WARRANTY; without even the implied warranty of
-## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-## GNU General Public License for more details.
- 
-## You should have received a copy of the GNU General Public License
-## along with this program; if not, write to the Free Software
-## Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-
-## NOTE: sorry I didn't have time to modify this as I promised.
+# Copyright (C)2013 Laurentiu Badea
 
 """logging service using syslog, informative exception stacktraces.
 
