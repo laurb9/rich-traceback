d57dcf383dde04fa47596f99b1a7e23e95138fef
Laurentiu Badea <laurb9@gmail.com>
2015-06-09 23:15:27 -0700

    Replace setup.py with one based on setuptools, rename tests to test, removed old test.
    Housekeeping

--- setup.py
+++ setup.py
@@ -1,17 +1,41 @@
 #!/usr/bin/env python
+"""rich traceback setup
+ 
+based on https://packaging.python.org/en/latest/distributing.html
+"""
 
-from distutils.core import setup
+from setuptools import setup, find_packages
 
-setup(name='python-rich-traceback',
-      version='1.0',
-      description='Rich Traceback Logger',
-      long_description=('Informative Traceback Logging for Python\n\n'
-                        'Informative stack traces showing method parameters\n'
-                        'Simple standalone logger with console syslog support.\n'
-                        ),
-      license='GPLv2',
-      author='Laurentiu Badea',
-      author_email='laurb9+rich-traceback@gmail.com',
-      url='https://github.com/laurb9/rich-traceback',
-      packages=['rich_traceback'],
-     )
+setup(
+    name='python-rich-traceback',
+    version='1.0.1b1',
+    description='Rich Traceback Logger',
+    long_description=('Informative Traceback Logging for Python\n\n'
+                      'Informative stack traces showing method parameters\n'
+                      'Simple standalone logger with console syslog support.\n'
+                      ),
+    license='Apache License 2.0',
+    author='Laurentiu Badea',
+    author_email='laurb9+rich-traceback@gmail.com',
+    url='https://github.com/laurb9/rich-traceback',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Environment :: No Input/Output (Daemon)',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2',
+    ],
+
+    keywords='traceback log syslog informative stack traces',
+
+    packages=['rich_traceback'],
+    install_requires=[],
+
+    extras_require={
+        'dev': ['check-manifest'],
+        'test': ['coverage'],
+    },
+)
